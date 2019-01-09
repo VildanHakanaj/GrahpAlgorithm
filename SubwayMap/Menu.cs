26bb233238b15a2a50d94282474f6e005baea685
Vildan Hakanaj <vildanhakanaj@trentu.ca>
2019-01-09 00:34:16 -0500

    Added a dynamic menu to the system

---
+++ SubwayMap/Menu.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubwayMap
+{
+    class Menu
+    {
+        List<string> menuItemsss = new List<string>(){
+                /*[1]*/"Add Station",
+                /*[2]*/"Add Link",
+                /*[3]*/"Remove Station",
+                /*[4]*/"Remove Link",
+                /*[5]*/"Print the Graph",
+                /*[6]*/"Clear Menu",
+                /*[7]*/"Exit",
+            };
+        string selection;
+        int index = 0;
+
+        public void ShowMenu(SubwayMap<char> map)
+        {
+            while (true)
+            {
+                selection = SelectMenu();
+                if (selection.Length != 0)
+                {
+                    Console.CursorVisible = true;
+                    ExecuteSelection(selection, map);
+                }
+
+            }
+        }
+
+
+        private void ExecuteSelection(string selection, SubwayMap<char> map)
+        {
+            switch (selection)
+            {
+                case "Add Station":
+                    char station;
+                    Console.Write("Enter a name for the station ==> ");
+                    if (char.TryParse(Console.ReadLine(), out station))
+                    {
+                        map.InsertStation(station);
+                        Console.Clear();
+                        Console.WriteLine("Just inserted station [ {0} ] into the graph", station);
+                        wait();
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nInvalid input! Needs to be a character\n");
+                    }
+                    break;
+
+                case "Add Link":
+                    Console.WriteLine("Selected the second option\n");
+                    break;
+                case "Remove Station":
+                    Console.WriteLine("Selected the third option\n");
+                    break;
+                case "Remove Link":
+                    Console.WriteLine("Selected the fourth option\n");
+                    break;
+                case "Print the Graph":
+                    Console.Clear();
+                    map.PrintGraph();
+                    Console.WriteLine("s", 3);
+                    break;
+                case "Clear Menu":
+                    Console.WriteLine("Selected the sixth option");
+                    break;
+                case "Exit":
+                    Environment.Exit(1);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void wait()
+        {
+            Console.WriteLine("Press enter to continue...");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        private string SelectMenu()
+        {
+
+
+            Console.CursorVisible = false;
+            for (int i = 0; i < menuItemsss.Count; i++)
+            {
+                if (i == index)
+                {
+                    Console.BackgroundColor = ConsoleColor.Gray;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine(menuItemsss[i]);
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.WriteLine(menuItemsss[i]);
+                }
+            }
+
+            if (DetectKey())
+            {
+                return menuItemsss[index];
+            }
+
+            Console.Clear();
+            return "";
+        }
+
+        public bool DetectKey()
+        {
+            ConsoleKeyInfo ckey = Console.ReadKey();
+            if (ckey.Key == ConsoleKey.DownArrow)
+            {
+                if (index == menuItemsss.Count - 1)
+                {
+                    index = 0;
+                }
+                else
+                {
+                    index++;
+                }
+            }
+            else if (ckey.Key == ConsoleKey.UpArrow)
+            {
+                if (index == 0)
+                {
+                    index = menuItemsss.Count - 1;
+                }
+                else
+                {
+                    index--;
+                }
+            }
+            else if (ckey.Key == ConsoleKey.Enter)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
--- SubwayMap/Program.cs
+++ SubwayMap/Program.cs
@@ -11,70 +11,13 @@ namespace SubwayMap
         static void Main(string[] args)
         {
 
-            SubwayMap<char> subway = new SubwayMap<char>();
-            ShowMenu();
-            int lineNumber;
-            do
-            {
-                 lineNumber = Convert.ToInt32(Console.ReadLine());
-                Answer(lineNumber, subway);
-            } while (lineNumber != 6);
+            SubwayMap<char> map = new SubwayMap<char>();
+            Menu menu = new Menu();
 
-            subway.PrintGraph();
-
-            Console.ReadKey();
-
+            menu.ShowMenu(map);
         }
 
-        public static void ShowMenu()
-        {
-            Console.WriteLine("Enter the line number\n\n");
-            Console.WriteLine("1) Insert Station");
-            Console.WriteLine("2) Remove Station");
-            Console.WriteLine("3) Insert Link");
-            Console.WriteLine("4) Remove Link");
-            Console.WriteLine("5) Exit");
-        }
 
-        public static void Answer(int lineNumber, SubwayMap<char> subway)
-        {
-
-            switch (lineNumber)
-            {
-                case 1:
-                    Console.Write("Enter a station name: ");
-                    char input = Convert.ToChar(Console.ReadLine());
-                    subway.InsertStation(input);
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    char from, to;
-                    string color;
-                    Console.Write("Enter from: ");
-                    from = Convert.ToChar(Console.ReadLine());
-                    Console.Write("\nEnter to: ");
-                    to = Convert.ToChar(Console.ReadLine());
-                    Console.Write("\nEnter the link color: ");
-                    color = Console.ReadLine();
-                    subway.InsertLink(from, to, color);
-                    break;
-                case 4:
-                    break;
-                case 5:
-                    Console.Clear();
-                    ShowMenu();
-                    break;
-                case 6:
-                    lineNumber = 6;
-                    break;
-                case 7:
-                    subway.PrintGraph();
-                    break;
-                default:
-                    Console.WriteLine("Please enter a correct line number");
-                    break;
-            }
-        }
+       
     }
 }
